9e3b3317fe38f37c78bba515c4b98572220d1743
raluka <badoi.raluca@gmail.com>
2013-10-05 17:57:48 +0200

    add stuff

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,14 +1,6 @@
 class DefaultController < ApplicationController
 
   def home
-    #start point of the app, the name will be changed in the routes.rb files!!!
-    #TODO change the name in routes.rb
-    #TODO get the ip from user, and try see the location and/or is a new or return user
-
-
-    @query = params[:query]
-    #TODO create the result as in models
-
   end
 
 
