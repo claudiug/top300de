6c801456618bbb4cbd256652f6bb83abc6b910f3
raluka <badoi.raluca@gmail.com>
2013-10-05 16:00:30 +0200

    add compass and change application.css in application.css.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Top300de::Application.routes.draw do
+  get "default/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
