549077b68c7458a751c3cd47a9ab8c7372eee54c
raluka <badoi.raluca@gmail.com>
2013-10-05 16:01:20 +0200

    add compass and change application.css in application.css.scss

---
+++ app/controllers/default_controller.rb
@@ -0,0 +1,7 @@
+class DefaultController < ApplicationController
+  def home
+    #start point of the app, the name will be changed in the routes.rb files!!!
+    #TODO change the name in routes.rb
+    #TODO get the ip from user, and try see the location and/or is a new or return user
+  end
+end
