878b7c82caa145da86cd79f0d6b27fa9b2767bed
Vlad Mihai <mihaivladro@yahoo.com>
2013-10-06 19:21:12 +0200

    adaugat stiluri pentru search - partial

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -5,6 +5,12 @@
 
 $(document).ready(function() {
     resizeAd();
+    setupLabel();
+
+    $('.options .styled').click(function () {
+        setupLabel();
+        $(this).click(clearSelection);
+    });
 
     $(window).resize(function() {
         resizeAd();
@@ -16,3 +22,39 @@ function resizeAd() {
     var slideHeight = $(window).height() - $('header').height();
     $('.homepageEngine').height(slideHeight);
 }
+
+function setupLabel() {
+    if ($('.styled').length) {
+        $('.options label').each(function () {
+            $(this).removeClass('styled-on');
+        });
+        $('.styled input:checked').each(function () {
+            $(this).parent('label').addClass('styled-on');
+        });
+    };
+};
+
+$(function() {
+
+    $(".pnav").on("click", function() {
+
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+
+        if ($button.text() == "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 1) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+
+        $button.parent().find("input").val(newVal);
+        $button.parent().find(".number span").html(newVal);
+
+    });
+
+});
