991ba80fe5ecb620ec75e5afd0bc05250b8c235f
Vlad Mihai <mihaivladro@yahoo.com>
2013-10-06 14:18:23 +0200

    header, background si search

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,3 +1,18 @@
 /**
  * Created by vladmihai on 10/5/13.
  */
+
+
+$(document).ready(function() {
+    resizeAd();
+
+    $(window).resize(function() {
+        resizeAd();
+    });
+
+});
+
+function resizeAd() {
+    var slideHeight = $(window).height() - $('header').height();
+    $('.homepageEngine').height(slideHeight);
+}
