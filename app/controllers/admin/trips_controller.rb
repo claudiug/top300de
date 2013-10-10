04925b4b5f0385ba841b9e22c1a433f7a14796d8
claudiu <claudiu.garba@gmail.com>
2013-10-11 01:56:09 +0200

    fix the params for checkbox

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -4,7 +4,7 @@ class Admin::TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   def index
-    @trip = Trip.all
+    @trips = Trip.all
   end
 
   def new
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ActiveRecord::Base
   has_many :hotels
   has_many :restaurants
   has_and_belongs_to_many :categories
+
+
+
+
 end
