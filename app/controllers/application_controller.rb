3519d5ff61158260d4015c124244781f40acfd30
claudiu <claudiu.garba@gmail.com>
2013-10-10 11:04:44 +0200

    add method for checking if the user has the rights for accesing /create/new.edit/delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def request_login
+    if current_user.nil?
+      flash[:error] = "must be login"
+      redirect_to root_url
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :request_login
+
+
 end
