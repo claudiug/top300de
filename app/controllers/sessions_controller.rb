a4aa825cc6e221031aca3fe319780ed32449fda6
claudiu <claudiu.garba@gmail.com>
2013-10-10 10:58:01 +0200

    forget user && user.authenticate. Is possible to have a null user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.where(email: params[:login[:email]]).first
-    if user.authenticate(params[:login][:password])
+    if user && user.authenticate(params[:login][:password])
       session[:user_id] = user.id
       flash[:notice] = "Login! with #{user.name}"
       redirect_to root_path
