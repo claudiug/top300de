82ab557d52038533f1f22952dcdfc185d4ab5bd4
claudiu <claudiu.garba@gmail.com>
2013-10-18 21:16:08 +0200

    fixing

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,7 +7,20 @@ class Admin::DashboardController < ApplicationController
 
   def index
 
+    if params[:latitude] && params[:longitude]
+      get_temperature(params[:latitude], params[:longitude])
+    end
+
   end
+
+
+  private
+  def get_temperature(latitude, longitude)
+    @forecast ||= ForecastIO.forecast(latitude, longitude,             #do not use spaces in exclude clause!!!
+                                      params: { units: 'si', exclude: 'minutely,hourly,flags,alerts' })
+
+  end
+
 
 
 end
---
+++ config/initializers/forecast.rb
@@ -0,0 +1,4 @@
+require 'forecast_io'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE   #THIS IS SO BAD IN SO MANY WAYS
+ForecastIO.api_key = '7ad69e74eb23f877270fa35ddfd82dbc'
+
