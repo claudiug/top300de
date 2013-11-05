e830b95e9c33236829f6fe8d26cd7259f55118b2
claudiu <claudiu.garba@gmail.com>
2013-11-05 14:03:47 +0100

    some methods for count messages for mailbox and count messages from user

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -1,4 +1,15 @@
 class Mailbox < ActiveRecord::Base
   belongs_to :account
   has_many :messages
+
+
+  def count_messages
+    messages.size
+  end
+
+
+  def count_message_from_user(user)
+     messages.where(from: "#{user}").size
+  end
+
 end
