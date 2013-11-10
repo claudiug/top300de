26bdaf4f2923275fe74d5caa1fad2589b1f4c0a4
claudiug <claudiu.garba@gmail.com>
2013-11-10 20:18:20 +0100

    add trip factory object

--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :category do
     name "category"
-    is_active :true
+    is_active true
     seo "some seo"
     description "description"
   end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe "Trip" do
 
-  it 'is valid with : name, city, zip_code, description, is_active'
+  it 'is valid with : name, city, zip_code, description, is_active' do
+    expect(build(:trip)).to be_valid
+  end
   it 'is invalid without name'
   it 'is invalid without city'
   it 'is invalid without zip_code'
