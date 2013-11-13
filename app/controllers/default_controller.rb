8148c3677fb2a027fffcebeb01bf1a2925ac8646
claudiug <claudiu.garba@gmail.com>
2013-11-13 17:12:38 +0100

    add methods for static pages

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -5,5 +5,29 @@ class DefaultController < ApplicationController
   def home
   end
 
+  def contact_us
+
+  end
+
+  def join_us
+
+  end
+
+  def term
+
+  end
+
+  def privacy
+
+  end
+
+  def sitemap
+
+  end
+
+  def contribute
+
+  end
+
 
 end
