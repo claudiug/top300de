d203d99e721838ac2975d7296943b8662b918e2d
claudiug <claudiu.garba@gmail.com>
2013-11-14 01:12:01 +0100

    not working...

---
+++ spec/features/trips_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'Trips' do
+  scenario 'make a trip search' do
+
+    visit root_path
+    find(:css, '#go_like').set(true)   #click on the checkbox
+    click_button 'Show me'
+    expect(page).to have_content 'Result'
+
+  end
+
+end
