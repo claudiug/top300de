a094768ffb078c7de1db142788ef812223ae175e
claudiug <claudiu.garba@gmail.com>
2013-11-16 17:07:52 +0100

    changes

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -7,6 +7,11 @@ class DefaultController < ApplicationController
 
   def contact_us
 
+    name = params[:name] if params[:name].present?
+    email = params[:email] if params[:email].present?
+    content = params[:content] if params[:content].present?
+    @email = [name, email, content]
+    ContactMailer.create_feedback_user(@email).deliver
   end
 
   def join_us
