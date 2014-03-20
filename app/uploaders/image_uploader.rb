ada8d48f3a58ecc00c452e689aedb5897ad40df5
vladstt <mihaivladro@yahoo.com>
2014-03-20 19:15:18 +0100

    added locale to header url, fixed image rotation on upload, hide h1 when empty, removed ger_capital validation & others

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,11 +10,22 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+   process :rotate
+   def rotate
+     manipulate! do |img|
+       img.auto_orient
+       img = yield(img) if block_given?
+       img
+     end
+   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
